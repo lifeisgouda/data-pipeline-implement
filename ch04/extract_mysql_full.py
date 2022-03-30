6352d94958d4cb7da48bf7cb4184b52b99ab4683
Jane Jeon <dahaejeon@gmail.com>
2022-03-30 22:03:34 +0900

    all

---
+++ ch04/extract_mysql_full.py
@@ -0,0 +1,54 @@
+import pymysql
+import csv
+import boto3
+import configparser
+
+# MySQL 연결을 초기화
+parser = configparser.ConfigParser()
+parser.read("pipeline.conf")
+hostname = parser.get("mysql_config", "hostname")
+port = parser.get("mysql_config", "port")
+username = parser.get("mysql_config", "username")
+dbname = parser.get("mysql_config", "database")
+password = parser.get("mysql_config", "password")
+
+conn = pymysql.connect(host=hostname,
+        user=username,
+        password=password,
+        db=dbname,
+        port=int(port))
+
+if conn is None:
+  print("Error connecting to the MySQL database")
+else:
+  print("MySQL connection established!")
+
+# Order table 전체추출 실행
+  m_query = "SELECT * FROM Orders;"
+local_filename = "order_extract.csv"
+
+m_cursor = conn.cursor()
+m_cursor.execute(m_query)
+results = m_cursor.fetchall()
+
+# 전체 추출한 내용을 파이프로 구분된 CSV에 씀
+with open(local_filename, 'w') as fp:
+  csv_w = csv.writer(fp, delimiter='|')
+  csv_w.writerows(results)
+
+fp.close()
+m_cursor.close()
+conn.close()
+
+# load the aws_boto_credentials values
+parser = configparser.ConfigParser()
+parser.read("pipeline.conf")
+access_key = parser.get("aws_boto_credentials", "access_key")
+secret_key = parser.get("aws_boto_credentials", "secret_key")
+bucket_name = parser.get("aws_boto_credentials", "bucket_name")
+
+s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+
+s3_file = local_filename
+
+s3.upload_file(local_filename, bucket_name, s3_file)
